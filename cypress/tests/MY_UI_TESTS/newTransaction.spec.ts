03fb58b837a19b815307d03b68f6d1a557026f86
vivekDebugs <vivekkumar.1644@gmail.com>
2021-10-18 14:59:33 +0530

    written tests for new transaction

--- cypress/tests/MY_UI_TESTS/newTransaction.spec.ts
+++ cypress/tests/MY_UI_TESTS/newTransaction.spec.ts
@@ -1,21 +1,71 @@
 /// <reference types='cypress' />
 
 describe("NEW TRANSACTION", () => {
-  it("should take the user to new transaction route", () => {
-    cy.visit("http://localhost:3000/");
-    const userInfo = {
-      firstName: "Arely",
-      lastName: "Kertzmann",
-      username: "Tavares_Barrows",
-      password: "s3cret",
-    };
+  const userInfo = {
+    firstName: "Arely",
+    lastName: "Kertzmann",
+    username: "Tavares_Barrows",
+    password: "s3cret",
+  };
 
+  const receiverInfo = {
+    firstName: "Edgar",
+    lastName: "Johns",
+    username: "Katharina_Bernier",
+    password: "s3cret",
+  };
+
+  const paymentInfo = {
+    amount: "10",
+    description: "Candies",
+  };
+
+  beforeEach(function () {
+    cy.task("db:seed");
+
+    cy.intercept("GET", "/users*").as("allUsers");
+
+    cy.intercept("GET", "/users/search*").as("usersSearch");
+
+    cy.intercept("POST", "/transactions").as("createTransaction");
+
+    cy.visit("http://localhost:3000/");
     cy.login(userInfo.username, userInfo.password);
     cy.log("USER LOGGED IN");
+  });
+
+  it("should take the user to new transaction route", () => {
     cy.getBySel("sidenav-newTransaction").click();
     cy.log("USER CLICKED ON NEW TRANSACTON");
     cy.url().should("include", "/transaction/new");
 
     cy.contains(`${userInfo.firstName}`).should("exist");
+  });
+
+  it("should go to new Transaction, select a user and initiate transaction, and check transaction history for the transaction made", () => {
+    cy.getBySel("sidenav-newTransaction").click();
+    cy.wait("@allUsers");
+    cy.getBySel("user-list-search-input").type(
+      `${receiverInfo.firstName} ${receiverInfo.lastName}`,
+      { force: true }
+    );
+    cy.wait("@usersSearch");
+    cy.getBySelLike("user-list-item").contains(receiverInfo.firstName).click({ force: true });
+    cy.getBySelLike("amount-input").type(paymentInfo.amount);
+    cy.getBySelLike("description-input").type(paymentInfo.description);
+    cy.visualSnapshot("Amount and Description Input");
+    cy.getBySelLike("submit-payment").click();
+
+    cy.wait(["@createTransaction", "@getUserProfile"]);
+    cy.getBySel("alert-bar-success")
+      .should("be.visible")
+      .and("have.text", "Transaction Submitted!");
+
+    cy.getBySel("sidenav-home").click();
+    cy.getBySel("nav-personal-tab").click();
+    cy.contains(
+      `${userInfo.firstName} ${userInfo.lastName} paid ${receiverInfo.firstName} ${receiverInfo.lastName}`
+    ).should("exist");
+    cy.contains(paymentInfo.description).should("exist");
   });
 });
---
+++ cypress/tests/MY_UI_TESTS/readme.md
@@ -0,0 +1,24 @@
+# Tests for new Transactions
+
+## Sign In
+
+![ssOne](https://user-images.githubusercontent.com/49686162/137702947-7bb6ebc6-d525-4042-a459-9e41fbd74cf4.png)
+
+## New Transaction
+
+![ssTwo](https://user-images.githubusercontent.com/49686162/137702927-7e15b4ab-ce68-4d7a-a5f1-3eaedf63dcdc.png)
+
+## Select a user
+
+![ssThree](https://user-images.githubusercontent.com/49686162/137702934-cb616b5b-ffe5-4628-9ddd-ec501e81cbbe.png)
+![ssFour](https://user-images.githubusercontent.com/49686162/137702917-2af02bb8-7a2e-4cd6-8c52-40a1275f0794.png)
+
+## Enter Transaction details and pay
+
+![ssFive](https://user-images.githubusercontent.com/49686162/137702931-9bf1b028-9266-400b-91c3-0aefa0154b89.png)
+
+## Personal Transactions
+
+![ssSix](https://user-images.githubusercontent.com/49686162/137702932-cb49c8de-c98a-4a68-b918-b0c0e414d606.png)
+![ssSeven](https://user-images.githubusercontent.com/49686162/137702939-56f7edd5-f3f5-4e63-b6dc-e499ab881bd2.png)
+![ssEight](https://user-images.githubusercontent.com/49686162/137704754-cdd7f978-60a0-4f63-ae23-9b4291a0529a.png)
